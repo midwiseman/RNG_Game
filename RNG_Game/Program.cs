a6bfcdeea0dcc51cde92a885da5085b3d1494c06
Michael Wiseman <mi.d.wiseman@gmail.com>
2021-03-30 03:13:15 -0400

    Adding more desirable dialogue

--- RNG_Game/Program.cs
+++ RNG_Game/Program.cs
@@ -6,12 +6,16 @@ namespace RNG_Game
     {
         static string randomNumberGeneration()
         {
-            var question = "What do you believe will be the next random number generated? *** Hint: It's between 1 and 5.";
+            var question = "What will be the next random number generated?";
+            var hint = "Hint: It's between 1 and 5.";
+            var exit = "Type 'e' to exit.";
             Console.WriteLine(question);
+            Console.WriteLine(hint);
+            Console.WriteLine(exit);
             var userAnswer = Console.ReadLine();
             var userAnswerParsed = int.Parse(userAnswer);
             Random rnd = new Random();
-            int randomNumber = rnd.Next(0, 6);
+            int randomNumber = rnd.Next(1, 5);
             var success = "Correct! You got it right! Good job!";
             var failure = "Incorrect. The correct answer was ";
             if (randomNumber == userAnswerParsed)
@@ -32,7 +36,10 @@ namespace RNG_Game
                     correctAnswers = correctAnswers + 1;
                     totalAttempts = totalAttempts + 1;
                 } else { totalAttempts = totalAttempts + 1; }
-                Console.WriteLine(cycle + "** Current Score: " + " ** Correct Answers: " + correctAnswers + "** Total Attempts: " + totalAttempts);
+                Console.WriteLine(cycle);
+                Console.WriteLine("***Current Score***");
+                Console.WriteLine("Correct Answers: " + correctAnswers);
+                Console.WriteLine("Total Attempts: " + totalAttempts);
             }
         }
     }
